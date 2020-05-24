6df79387948449f4fcff575cca5b2e547b217fc5
shiva-r <shiva.r@iircade.com>
2020-05-24 16:18:22 +0530

    Add project files.

---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.EntityFramework
+{
+    public   class SimpleTraderDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(s => s.Stock);
+            base.OnModelCreating(modelBuilder); 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=127.0.0.1\\SQLEXPRESS;database=SimpleTraderDb;user=sa;password=Siva@123");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
